165f0325a5372feb4a9e321de5506334561ba346
Emile Drablant <emiledrablant@proton.me>
2024-10-17 15:35:22 -0300

    works with basic requirements

--- script.js
+++ script.js
@@ -1,26 +1,72 @@
-let firstNumber;
-let secondNumber;
-let operator;
-let numberDisplayed = 0;
+let firstNumber = '';
+let secondNumber = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const mult = document.querySelector(".mult");
+const divi = document.querySelector(".divi");
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", evaluate);
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearMemory);
 
 const numerals = document.querySelectorAll(".numeral");
+const operators = document .querySelectorAll(".operator");
 function bindButtons() {
     numerals.forEach(element => {
-        element.addEventListener("click", () => displayNumber(element.textContent));
+        element.addEventListener("click", () => appendNumber(element.textContent));
+    });
+    operators.forEach(element => {
+        element.addEventListener("click", () => setOperation(element.textContent));
     });
 }
 
-const screen = document.querySelector(".screen");
-function displayNumber(number) {
-    if (numberDisplayed === 0) {
-        numberDisplayed = number;
-    } else {
-        numberDisplayed += number;
+function setOperation(operator) {
+    if (currentOperation !== null) {
+        evaluate();
     }
-    screen.textContent = numberDisplayed;
+    firstNumber = screen.textContent;
+    currentOperation = operator;
+    shouldResetScreen = true;
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) {
+        return;
+    }
+    secondNumber = screen.textContent;
+    screen.textContent = operate(currentOperation, firstNumber, secondNumber);
+    currentOperation = null;
+}
+
+const screen = document.querySelector(".screen");
+function appendNumber(number) {
+    if (screen.textContent === "0" || shouldResetScreen) {
+        resetScreen();
+    }
+    screen.textContent += number;
+}
+function resetScreen() {
+    screen.textContent = "";
+    shouldResetScreen = false;
+}
+
+function clearMemory() {
+    screen.textContent = "0";
+    shouldResetScreen = false;
+    firstNumber = '';
+    secondNumber = '';
+    currentOperation = null;
 }
 
 function operate(operator, first, second) {
+    first = Number(first);
+    second = Number(second);
+
     switch (operator) {
         case "+":
             return add(first, second);
@@ -46,8 +92,11 @@ function multiply(first, second) {
 }
 
 function divide(first, second) {
-    return first / second;
+    if (second === 0) {
+        return 0;
+    } else {
+        return first / second;
+    }
 }
 
 bindButtons();
-displayNumber(0);
