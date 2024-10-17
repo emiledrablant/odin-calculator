84a2fdf7864673faf955f42613a59cf12997851a
Emile Drablant <emiledrablant@proton.me>
2024-10-17 09:18:49 -0300

    add basic logic for operations

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
