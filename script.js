9403113c2210a37acc2684d08d1beb8776fd474f
Emile Drablant <emiledrablant@proton.me>
2024-10-17 09:26:58 -0300

    add operate function to handle basic logic

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+function operate(operator, first, second) {
+    switch (operator) {
+        case "+":
+            return add(first, second);
+        case "-":
+            return subtract(first, second);
+        case "*":
+            return multiply(first, second);
+        case "/":
+            return divide(first, second);
+    }
+}
+
+console.log(operate("*", 10, 5));
+
 function add(first, second) {
     return first + second;
 }
